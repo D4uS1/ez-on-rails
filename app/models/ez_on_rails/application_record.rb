760fa11afd269c077947ed5ab479ba1362ce3e7e
Andreas Dausenau <andreasdausenau@Laptop-von-Andreas.fritz.box>
2024-11-06 19:16:43 +0100

    Fixed accidently lost content of ez-on-rails application record.

--- app/models/ez_on_rails/application_record.rb
+++ app/models/ez_on_rails/application_record.rb
@@ -1,5 +1,16 @@
 module EzOnRails
   class ApplicationRecord < ActiveRecord::Base
+    include EzOnRails::FullRansackSearchableConcern
+
     self.abstract_class = true
+
+    TABLE_PREFIX = 'eor_'
+
+    # Returns an array of attributes that are searchable for this record.
+    # Used by the subclasses in scoped_search.
+    # Can be overridden by subclasses, to provide alternative or additional search keys.
+    def self.search_keys
+      columns_hash.keys.map(&:to_sym)
+    end
   end
 end
