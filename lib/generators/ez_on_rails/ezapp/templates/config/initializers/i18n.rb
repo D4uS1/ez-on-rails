2213d7c8f841d07ca9cc5aa174ee5b9c04bfaaca
Andreas Dausenau <andreasdausenau@MacBookPro.fritz.box>
2025-02-03 18:49:20 +0100

    Fixed rubocop offense.

--- lib/generators/ez_on_rails/ezapp/templates/config/initializers/i18n.rb
+++ lib/generators/ez_on_rails/ezapp/templates/config/initializers/i18n.rb
@@ -2,5 +2,5 @@
 
 # Tell rails not only to search in the root directory of the locals. It should
 # also search in all subdirectories.
-Rails.application.config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml')]
+Rails.application.config.i18n.load_path += Rails.root.glob('config/locales/**/*.yml')
 Rails.application.config.i18n.default_locale = :en
